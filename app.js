8137158cb8c1962c7f798973881980339a1765fc
Bernardo Ferreira <bernardoferreira12@hotmail.com>
2019-06-11 06:20:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	const overlay = document.getElementById('overlay');
 	const startButton = document.querySelector('.btn__reset');
 	const qwerty = document.getElementById('qwerty');
-	const button = qwerty.children;
 	const phrase = document.getElementById('phrase');
 	const misses = 0;
 	const phrasesArray = [
@@ -44,22 +43,24 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	addPhraseToDisplay(newPhrase);
-
-	function checkLetter(event) {
+	function checkLetter(button) {
 		const letters = document.getElementsByClassName('letter');
-		for (let i = 0; i < letter.length; i++) {
-			if (event.target.textContent === letter[i].textContent) {
-				const li = document.querySelector('ul li');
-				li.className = 'show';
-				const letter = letter[i];
-				return letter[i];
-			} else {
-    			return null;
-    		}
+		let match;
+		for (let i = 0; i < letters.length; i++) {
+			let show = letters[i].textContent.toLowerCase();
+			if (show === button.textContent.toLowerCase()) {
+				console.log("match");
+				letters[i].className += 'show';
+				match = show;
+			}
 		}
+		return match;
 	}
 
-	button.addEventListener('click', () => {
-		
+	qwerty.addEventListener('click', (e) => {
+		if(e.target.tagName == 'BUTTON') {
+			e.target.className = 'chosen';
+			checkLetter(e.target);
+		}
 	});
 });
